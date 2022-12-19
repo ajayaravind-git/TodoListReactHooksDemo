b71c46e86381b206cfa210aabeb869e2344b9688
Ajay Aravind <ajayaravind.me@gmail.com>
2022-12-19 22:30:35 +0530

    styling fix

--- src/Todo.js
+++ src/Todo.js
@@ -13,11 +13,11 @@ function Todo({ completed, task, removeTodos, id, Completed, todoEditing }) {
             <ListItemText onClick={() => { Completed(id); }} style={{ textDecoration: completed ? "line-through" : "", cursor: "pointer", color: completed ? "black" : "slateblue" }}>
                 {task}
             </ListItemText>
-            <ListItemSecondaryAction>
+            <ListItemText style={{ display: "flex", alignItems: "center", justifyContent: "end" }}>
 
                 <IconButton aria-label="Edit" onClick={() => { toggleEditing() }}><EditIcon /></IconButton>
                 <IconButton aria-label="Delete" onClick={() => { removeTodos(id) }}><DeleteIcon /></IconButton>
-            </ListItemSecondaryAction>
+            </ListItemText>
         </ListItem > : <TodoEditForm id={id} task={task} toggleEditing={toggleEditing} todoEditing={todoEditing} />}</>)
 }
 
